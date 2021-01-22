33390ff81b2f83938b4dceaaa55ae9e80d16876f
Neoito <you@example.com>
2021-01-22 14:54:46 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 3301;
+
+process.on('uncaughtException', (err) => {
+    console.error(`${(new Date()).toUTCString()} unhandledException:`, err);
+    process.exit(0);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.error(`${(new Date()).toUTCString()} unhandledRejection:`, err);
+});
+
+server.listen({ port }, () => console.log(
+    ` Server ready at http://localhost:${port}/api`,
+));
+
